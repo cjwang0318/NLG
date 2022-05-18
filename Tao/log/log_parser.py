ef751d88ec9dd45b066e5e66ce9ebabb1a63792c
Chieh-Jen Wang <cjwang0318@gmail.com>
2022-05-18 16:43:56 +0800

    dictionary construction remove all english and first version log parser

---
+++ Tao/log/log_parser.py
@@ -0,0 +1,26 @@
+import tool_box as tb
+
+if __name__ == '__main__':
+    log = []
+    path = "./log.txt"
+    dataset = tb.read_file(path, 0)
+    for line in dataset:
+        if "----- " in line:
+            keyword = ""
+            type = ""
+        if "原始關鍵字：" in line:
+            temp = line.split("：")
+            keyword = temp[1]
+            continue
+        if "生成模型類別：" in line:
+            temp = line.split("：")
+            type = temp[1]
+            continue
+        if type == "GPT2":
+            temp = line.split("\t")
+            #print(line)
+            if len(temp) == 2:
+                log.append(keyword + "\t" + temp[1] + "\n")
+            continue
+
+    tb.write_file("./log_parsed.txt", log)
--- Tao/vocab/dictionary_constraction.py
+++ Tao/vocab/dictionary_constraction.py
@@ -1,5 +1,6 @@
 import tool_box as tb
 
+
 def isEnglish(s):
     try:
         s.encode(encoding='utf-8').decode('ascii')
@@ -8,24 +9,26 @@ def isEnglish(s):
     else:
         return True
 
+
 def dictionary_generation(path):
-    dict=[]
-    dataset=tb.read_file(path,0)
+    dict = []
+    dataset = tb.read_file(path, 0)
     for line in dataset:
-        if len(line)<2: #判斷長度
+        if len(line) < 2:  # 判斷長度
             continue
-        elif(isEnglish(line)): #判斷是否為英文
+        elif (isEnglish(line)):  # 判斷是否為英文
             continue
         else:
-            #line=convert_s2c(line)
-            dict.append(line+",1\n")
-    tb.write_file("./dict.csv",dict)
+            # line=convert_s2c(line)
+            dict.append(line + ",1\n")
+    tb.write_file("./dict.csv", dict)
+
 
 if __name__ == '__main__':
-    path="./vocab_desc_only_normalized.txt"
+    path = "./vocab_desc_only_top100k.txt"
     dictionary_generation(path)
 
-    # keyword1 = "時尚風西裝"
+    # keyword1 = "[SEP]"
     # keyword2 = "裝aa"
     # keyword3 = "abc"
     # print(isEnglish(keyword1))
