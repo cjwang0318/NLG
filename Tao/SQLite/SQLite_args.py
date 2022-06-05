0931b4eb45c197bebc0d20e060ddb1661848c351
cjwang <cjwang0318@gmail.com>
2022-06-05 22:37:57 +0800

    keyword extraction search version 0.1

--- Tao/SQLite/SQLite_args.py
+++ Tao/SQLite/SQLite_args.py
@@ -1,7 +1,16 @@
 database = './tao_seg_cht.db'  #
 nsamples = 5
-#search_type='exactly_search'
-search_type='seg_search'
+'''
+exactly_search：搜尋必需完全包含全部關鍵字
+seg_search：搜尋前先斷詞，例如：AA BB CC，先完全比對(AA BB CC)，依序縮減(BB CC)，最後(CC)
+key_search：搜尋前先斷詞，再做關鍵字擷取(AA BB CC, 順序根據演算法所提供的權重)，擷取幾個關鍵字根據nkeywords的設定，然後再使用模糊搜尋`itemName` LIKE '%AA%' AND `itemName` LIKE '%BB%'，
+            若沒有就縮短為 itemName` LIKE '%AA%' 
+'''
+# search_type='exactly_search'
+search_type = 'seg_search'
+# search_type = 'key_search'
 segmentation_server_IP = "192.168.50.29:5000"
+keyword_extraction_server_IP = "192.168.50.29:5000"
+nkeywords = 3
 dictionary = "tao_top100K"
-NLG_operation=True
+NLG_operation = True
--- Tao/SQLite/sql_search.py
+++ Tao/SQLite/sql_search.py
@@ -3,11 +3,10 @@ from datetime import datetime
 import random
 import sys
 import time
-import CKIP as cs
+import Call_API as cs
 
 sys.path.append("..")
 import tool_box as tb
-
 
 
 def insert_data(cursor):
@@ -28,9 +27,7 @@ def list_shuffle(list):
     return list
 
 
-def get_result(cursor, keyword, topk):
-    query = "SELECT `description` FROM description WHERE `description` LIKE '%" + keyword + "%'"
-    # query = "SELECT * FROM description WHERE `description` LIKE 'qq'"
+def get_result(cursor, query, topk):
     # print(query)
     results = cursor.execute(query)
     list = []
@@ -64,16 +61,60 @@ def generate_candidate_keyword_list(keyword):
     return seg_keywords, keyword_list
 
 
+def query_construction(keyword_list):
+    query = "SELECT * FROM `itemName_ID` WHERE "
+    keyword_len = len(keyword_list)
+    tmp = ""
+    i = 1
+    for item in keyword_list:
+        str = "`itemName` LIKE '%" + item + "%'"
+        if i < keyword_len:
+            tmp = tmp + str + " AND "
+        else:
+            tmp = tmp + str
+        i = i + 1
+    return query + tmp
+
+
+def generate_candidate_query_list(keyword):
+    seg_keywords = cs.call_CKIP(keyword)
+    keyword_list = cs.call_keyword_extraction(seg_keywords)
+    keyword_len = len(keyword_list)
+    i = 0
+    result = []
+    while i < keyword_len:
+        temp = keyword_list[:keyword_len - i]
+        # print(temp)
+        str1 = ' '.join(temp)
+        query = query_construction(temp)
+        # print(query)
+        result.append(str1 + "\t" + query)
+        i = i + 1
+    return result
+
+
 def get_seg_result(cursor, keyword_list, topk):
     for keyword in keyword_list:
         # print(keyword)
-        results = get_result(cursor, keyword, topk)
-        results_remove_apace = []
+        query = "SELECT `description` FROM description WHERE `description` LIKE '%" + keyword + "%'"
+        results = get_result(cursor, query, topk)
+        results_remove_space = []
         if results != None:
             for item in results:
                 item = item.replace(" ", "")
-                results_remove_apace.append(item)
-            return keyword, results_remove_apace
+                results_remove_space.append(item)
+            return keyword, results_remove_space
+    return keyword, results
+
+
+def get_keyword_result(cursor, keyword_list, topk):
+    for item in keyword_list:
+        item_split = item.split("\t")
+        keyword = item_split[0]
+        query = item_split[1]
+        results = get_result(cursor, query, topk)
+        if results != None:
+            return keyword, results
     return keyword, results
 
 
@@ -92,8 +133,8 @@ def log_results(keyword, seg_keywords, keyword_without_oov, result, generate_typ
 
 
 if __name__ == '__main__':
-    db = sqlite3.connect('./tao_seg_cht.db')
-    cursor = db.cursor()
+    # db = sqlite3.connect('./tao_seg_cht.db')
+    # cursor = db.cursor()
     # results = select(cursor)
     # results = get_result(cursor, "bba", 3)
     # if results == None:
@@ -103,11 +144,10 @@ if __name__ == '__main__':
     # db.commit()
     # db.close()
     start = time.time()
-    seg_keywords, keyword_list = generate_candidate_keyword_list("超級無敵馬甲西裝")
+    # seg_keywords, keyword_list = generate_candidate_keyword_list("超級無敵馬甲西裝")
+    seg_keywords = generate_candidate_query_list("超級無敵馬甲西裝")
     print(seg_keywords)
     # print(keyword_list)
-    keyword, ans = get_seg_result(cursor, keyword_list, 5)
+    # keyword, ans = get_seg_result(cursor, keyword_list, 5)
     end = time.time()
     print('time: ', end - start)
-    print(keyword)
-    print(ans)
